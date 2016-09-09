c28ea809fc54e3903a7cc8dcf78d772bb578f6f3
Matthew <Matthew@Matthew-PC>
2016-09-09 18:55:20 -0500

    Initial commit

---
+++ desktop/src/com/slyvr/gamelings/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.slyvr.gamelings.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.slyvr.gamelings.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
